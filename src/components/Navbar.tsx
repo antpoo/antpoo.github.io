204ef6302ebb168200326f5d89f57b92b922a8db
Antpoo <1jiangand2@hdsb.ca>
2023-07-13 21:17:13 -0400

    Added a description for myself

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,18 +3,18 @@ import React from 'react';
 function Navbar() {
     
   return (
-    <header className="bg-red-900 md:sticky top-0 z-10">
-      <div className="container mx-auto flex flex-wrap p-1 flex-col md:flex-row items-center text-red-100">
-        <a className="title-font font-medium text-white mb-4 md:mb-0">
+    <header className="md:sticky top-0 z-10">
+      <div className="container mx-auto flex flex-wrap p-1 flex-col md:flex-row items-center text-gray-400">
+        <a className="title-font font-medium text-black mb-4 md:mb-0">
           <a href="#about" className="ml-3 text-xl">
             Andrew Jiang
           </a>
         </a>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-          <a href="#projects" className="mr-5 hover:text-white">
+          <a href="#projects" className="mr-5 hover:text-black">
             My Projects
           </a>
-          <a href="#achievements" className="mr-5 hover:text-white">
+          <a href="#achievements" className="mr-5 hover:text-black">
             My Achievements
           </a>
         </nav>
--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -11,7 +11,32 @@ function About() {
                     <h2 className='font-sans'>
                         PROGRAMMER, MATH ENJOYER, AND CASUAL GAMER
                     </h2>
+                    <p className="mb-8 leading-relaxed">
+                        <br />
+                        <br />
+                        <br />
+                        Hi! My name is Andrew Jiang. I am a 12th Grade student currently attending Dr. Frank J. Hayden Secondary School. <br />
+                        I am super passionate about STEM and love to learn whatever I can about it in my free time! I like to spend my time <br />
+                        solving math problems, competitive programming problems, and working on projects like this website! On another <br />
+                        note, I also do love world affairs and geopolitics, and I try and spend my time learning about the world and <br />
+                        understanding us as humans more. For this reason, I am super into Model United Nations as well, and have been <br />
+                        to many conferences where I can debate, discuss, negotiate, and solve world issues with many peers that I have <br />
+                        never met before! Also, I do like getting on and playing video games from time to time :D.
+                    </p>
+                    <div className="flex justify-center">
+                        <a
+                        href="#projects"
+                        className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+                        My Projects!
+                        </a>
+                        <a
+                        href="#contact"
+                        className="ml-4 inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+                        Work With Me
+                        </a>
+                    </div>
                 </div>
+                    
             </div>
         </section>
     );
